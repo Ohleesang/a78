853b3552a6fbf5a3878190dd82877f728b33f079
Ohleesang <jammmmed@gmail.com>
2023-11-23 10:29:38 +0900

    이 문제의 핵심은 오버플로우인거 같다. 피보나치수 값이 기하급수적으로 늘어나기때문에 BigInteger 클래스를 이용하니 해결 할 수 있다.

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,20 +1,29 @@
 // 피보나치 수열
-//
+// n의 범위 0~100000
+import java.math.BigInteger
 
 class Solution {
+    /*
     fun fibo(n: Int):Int{//재귀함수의 시간복잡도 O(2^n) >>다른방식으로 짜야한다
         if(n==0) return 0
         if(n==1) return 1
         return fibo(n-1)+fibo(n-2)
     }
+    */
     fun solution(n: Int): Int {
         var answer = 0
-        answer = fibo(n)%1234567
+        var listFibo = mutableListOf<BigInteger>()
+        listFibo.add(BigInteger("0"))
+        listFibo.add(BigInteger("1"))
+        for(i in 2..n){
+            listFibo.add(listFibo[i-1].add(listFibo[i-2]))
+        }
+        answer = (listFibo.last().remainder(BigInteger("1234567"))).toInt()
         return answer
     }
 }
 fun main(){
     var a = Solution()
-    a.solution(3)//2
+    a.solution(99999)//2
     a.solution(5)//5
 }
